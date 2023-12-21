7a703fa42fbbae6edb708c795b0c8d99afd5792f
Dennis Byberg <dennis.byberg@hotmail.com>
2023-12-21 21:10:18 +0100

    first commit

---
+++ day-1/index.ts
@@ -0,0 +1,40 @@
+function getSumOfAllCalibrationValues(localTxtFile: string): Promise<number> {
+  const fs = require("fs");
+  let sum: number = 0;
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(localTxtFile, "utf8", (err: any, data: any) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      const dataArray = data.split("\n");
+
+      for (let i = 0; i < dataArray.length; i++) {
+        let numbers: number[] = [];
+        for (let j = 0; j < dataArray[i].length; j++) {
+          if (!isNaN(parseInt(dataArray[i][j]))) {
+            numbers.push(parseInt(dataArray[i][j]));
+          }
+        }
+        let firstDigit = numbers[0];
+        let secondDigit: number;
+
+        if (numbers.length > 1) {
+          secondDigit = numbers[numbers.length - 1];
+        } else {
+          secondDigit = numbers[0];
+        }
+
+        let twoDigitNumber = parseInt(`${firstDigit}${secondDigit}`);
+        sum += twoDigitNumber;
+      }
+      resolve(sum);
+    });
+  });
+}
+
+getSumOfAllCalibrationValues("./day-1/input.txt")
+  .then((sum) => console.log(sum))
+  .catch((err) => console.error(err));
